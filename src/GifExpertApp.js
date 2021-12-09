8a6d22a0b2252aaaafb9298bd9e310a65797f644
Rosario Rustarazo <rosario.rustarazo@xauencybersecurity.com>
2021-12-09 11:55:56 +0100

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+
+
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    /* const handleAdd = () => {
+        //setCategories(['Hunter', ...categories]);
+        setCategories( cats => [...cats, 'Hunter']);
+    } */
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr />
+
+            
+            <ol>
+                {
+                    categories.map( cat => (
+                        <GifGrid 
+                            key={cat}
+                            category={cat}
+                        />
+                ))
+                }
+            </ol>
+        </>
+    )
+}
+
+export default GifExpertApp;
